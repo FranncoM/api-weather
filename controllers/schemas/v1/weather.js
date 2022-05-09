45979193ab54d19102042c200a918ddf9d06311e
Franco Miño <minofrancoj@gmail.com>
2022-05-09 03:45:23 -0300

    feat: add apilocate and openWeather api services and test route current

--- controllers/schemas/v1/weather.js
+++ controllers/schemas/v1/weather.js
@@ -1,12 +1,33 @@
+const dailySchema = {
+  dt: { type: 'number' },
+  sunrise: { type: 'number' },
+  sunset: { type: 'number' },
+  moonrise: { type: 'number' },
+  moonset: { type: 'number' },
+  moon_phase: { type: 'number' },
+  temp: { type: 'object' },
+  feels_like: { type: 'object' },
+  pressure: { type: 'number' },
+  humidity: { type: 'number' },
+  dew_point: { type: 'number' },
+  wind_speed: { type: 'number' },
+  wind_deg: { type: 'number' },
+  wind_gust: { type: 'number' },
+  weather: { type: 'array' },
+  clouds: { type: 'number' },
+  pop: { type: 'number' },
+  uvi: { type: 'number' }
+};
+
 const currentSchema = {
-  querystring: {
-    name: { type: 'string' }
-  },
   response: {
     200: {
       type: 'object',
       properties: {
-        status: { type: 'string' }
+        city: { type: 'string' },
+        timezone: { type: 'string' },
+        current: dailySchema,
+        daily: dailySchema
       }
     }
   }
---
+++ services/apiOpenWeather.js
@@ -0,0 +1,37 @@
+const fetch = require('node-fetch');
+const { OPENWEATHER_KEY: apiKey } = process.env;
+
+const baseUrl = 'https://api.openweathermap.org/data/2.5/';
+const geoUrl = 'http://api.openweathermap.org/geo/1.0/';
+
+const getForecast = async (lat, lon) => {
+  const response = await fetch(`${baseUrl}onecall?lat=${lat}&lon=${lon}&exclude=minutely,hourly&units=metric&appid=${apiKey}`);
+  const data = await response.json();
+
+  return data;
+};
+
+const getCurrent = async (lat, lon) => {
+  const response = await fetch(`${baseUrl}onecall?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`);
+  const data = response.json();
+  return data;
+};
+
+const getDirectGeoCoding = async (city) => {
+  const cityEncoded = encodeURIComponent(city);
+  const limit = 1;
+  const response = await fetch(`${geoUrl}direct?q=${cityEncoded}&limit=${limit}&appid=${apiKey}`);
+  const data = await response.json();
+
+  return data[0];
+};
+
+const getReverseGeoCoding = async (lat, lon) => {
+  const limit = 1;
+  const response = await fetch(`${geoUrl}direct?reverse?lat=${lat}&lon=${lon}&limit=${limit}&appid=${apiKey}`);
+  const data = response.json();
+
+  return data[0];
+};
+
+module.exports = { getForecast, getCurrent, getDirectGeoCoding, getReverseGeoCoding };
